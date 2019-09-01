eb3f3b317252aa84a769bc1e7690b68a1b9d1074
fako <mail@f-ko.eu>
2019-09-01 12:48:39 +0200

    Add health call to monitor example code

--- examples/monitor/main.go
+++ examples/monitor/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"net/http"
 	"time"
 
@@ -10,12 +11,21 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+// Health denotes the result of a health check
+type Health struct {
+	OK      bool
+	Details string
+}
+
+const maxDataAge = 2 * time.Minute
+
 // Simple global variables to hold configuration / data
 var (
 	devicePath     string
 	serverEndpoint string
 
 	currentData *usbwde.DataPoint
+	health      *Health
 )
 
 func main() {
@@ -43,6 +53,10 @@ func readLoop() {
 	defer func() {
 		if r := recover(); r != nil {
 			logrus.StandardLogger().Errorf("Panic recovered in readLoop(): %s", r)
+			health = &Health{
+				OK:      false,
+				Details: fmt.Sprintf("Panic recovered in readLoop(): %s", r),
+			}
 		}
 	}()
 
@@ -50,6 +64,10 @@ func readLoop() {
 	sensor, err := usbwde.New(devicePath)
 	if err != nil {
 		logrus.StandardLogger().Errorf("Error opening %s: %s", devicePath, err)
+		health = &Health{
+			OK:      false,
+			Details: fmt.Sprintf("Error opening %s: %s", devicePath, err),
+		}
 
 		return
 	}
@@ -62,10 +80,17 @@ func readLoop() {
 		dataPoint, err := sensor.Read()
 		if err != nil {
 			logrus.StandardLogger().Errorf("Error reading data from %s: %s", devicePath, err)
+			health = &Health{
+				OK:      false,
+				Details: fmt.Sprintf("Error reading data from %s: %s", devicePath, err),
+			}
 		}
 
 		// Assign newly read data to current data
 		currentData = dataPoint
+		health = &Health{
+			OK: true,
+		}
 	}
 }
 
@@ -85,6 +110,7 @@ func startServer() {
 
 	// Routes
 	e.GET("/", returnData)
+	e.GET("/health", returnHealth)
 
 	// Start server
 	logrus.StandardLogger().Fatal(e.Start(serverEndpoint))
@@ -99,4 +125,28 @@ func returnData(c echo.Context) error {
 	}
 
 	return c.JSONPretty(http.StatusOK, currentData, "  ")
+}
+
+// Health handler
+func returnHealth(c echo.Context) error {
+
+	// If there is no health (yet), signify via HTTP error
+	if health == nil {
+		return c.String(http.StatusNoContent, "No health data yet")
+	}
+
+	// If the current health is not ok, return it
+	if health.OK == false {
+		return c.JSONPretty(http.StatusOK, health, "  ")
+	}
+
+	// If the data is too old, return a warning
+	if currentData != nil && currentData.TimeStamp.Before(time.Now().Add(-2*time.Minute)) {
+		return c.JSONPretty(http.StatusOK, &Health{
+			OK:      false,
+			Details: fmt.Sprintf("Data is older than %v", maxDataAge),
+		}, "  ")
+	}
+
+	return c.JSONPretty(http.StatusOK, health, "  ")
 }
